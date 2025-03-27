ec0c749def11cd3d3e69bff6a0af02203d36e118
fhggbhjghy <fhggbhjghy11@gmail.com>
2025-03-26 21:54:51 -0400

    Implemented prop callback function and message event handler

--- src/app/live_chat_monitor.py
+++ src/app/live_chat_monitor.py
@@ -7,21 +7,79 @@ import logging
 logger = logging.getLogger(__name__)
 
 class Monitor():
-    def __init__(self):
+    def __init__(self, update):
+
+        # Passed callback function to handle danmu event updates
+        self.update = update
+
+        # Grab credential and login prepare chat hook
         self.credential = get_credentials()
         self.room_display_id = get_room_display_id()
         self.room = live.LiveDanmaku(
             room_display_id=self.room_display_id,
             credential=self.credential
         )
+
+        # Used to run process in sub-thread
         self.thread = None
 
+        # Used to id messages
+        self.id = 0
+
+        # Message event handler
+        @self.room.on('DANMU_MSG')
+        async def on_danmaku(event):
+
+            # Check if event body is valid message structure
+            data = event.get('data', None)
+            if not data:
+                return
+            info = data.get('info', None)
+            if not info:
+                return
+            if len(info) <= 2:
+                return
+            
+            # Parse message body for content, identifier, and chat rank
+            parsed_message_info = {}
+            parsed_message_info['message'] = info[1]
+            parsed_message_info['sender_uid'] = info[2][0]
+            parsed_message_info['sender_name'] = info[2][1]
+
+            # Check if user is equiping chat rank of current room, default to 0 if not
+            parsed_message_info['fan_id'] = -1
+            parsed_message_info['sender_rank'] = 0
+            if len(info) > 3 and len(info[3]) > 3:
+                parsed_message_info['fan_id'] = info[3][3]
+                if info[3][3] == self.room.room_display_id:
+                    parsed_message_info['sender_rank'] = info[3][0]
+
+            # Create structured text to be displayed on gui
+            parsed_message_info['structured_message'] = f'{parsed_message_info["sender_name"]} (lv.{parsed_message_info['sender_rank']}): {parsed_message_info["message"]}'
+
+            # Try calling callback method to update Gui
+            try:
+                self.update(self.id, parsed_message_info)
+                self.id = (self.id+1)%36
+            except Exception as e:
+                print(e)
+
+            # Debugging
+            print(f'{parsed_message_info["sender_name"]} lv.{parsed_message_info['sender_rank']} ({parsed_message_info["sender_uid"]}): {parsed_message_info["message"]}')
+
+
+    def launch_monitor(self):
+        sync(self.room.connect())
+
     def run(self):
+
+        # Spawn thread that connects to Bilibili danmu websocket
         try:
             self.thread = threading.Thread(
-                target = sync(self.room.connect()),
+                target = self.launch_monitor,
                 daemon = True
             )
+            self.thread.start()
         except Exception as e:
             logger.info(e)
             return False
--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from app import live_chat_monitor, room_control
 from app.utility import change_room_display_id
 from gui import user_interface
 
+def update(id, msg):
+    print(msg)
+
 if __name__ == '__main__':
     option = input('0 to close\n1 to start\n2 to change room\n')
     while option.isdigit():
@@ -12,9 +15,13 @@ if __name__ == '__main__':
                 break
             case 1:
                 print('Starting monitor')
-                monitor = live_chat_monitor.Monitor()
+                monitor = live_chat_monitor.Monitor(update)
                 monitor.run()
             case 2:
                 print('Changing room display id')
                 change_room_display_id()
+            case _:
+                print('Exiting...')
+                break
         option = input('0 to close\n1 to start\n2 to change room\n')
+    print('Exiting...')
